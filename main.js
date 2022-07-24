7bbe1d5ada8c84252bb7cc3d557c132645fe5bcf
benjflowers <benjaminjflowers@gmail.com>
2022-07-24 15:17:08 -0500

    idk

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 const mouseYDebug = document.getElementById('mouse-y');
+const mouseXDebug = document.getElementById('mouse-x');
 
 const getMousePos = (canvas, event) => {
   const rect = canvas.getBoundingClientRect();
@@ -23,6 +24,16 @@ const rect = (ctx, xPos, yPos, height, width) => {
   ctx.strokeRect(xPos, yPos, height, width)
 };
 
+const point = (ctx, xPos, yPos) => {
+  ctx.fillRect(xPos, yPos, 1, 1);
+}
+
+const ellipse = (ctx, x, y, r, sAngle = 0, eAngle = 2 * Math.PI) => {
+  ctx.beginPath();
+  ctx.arc(x, y, r, sAngle, eAngle);
+  ctx.fill();
+}
+
 const DIM = 4;
 const rows = [];
 
@@ -40,20 +51,26 @@ setFillStyle(ctx, 200, 200, 200);
 
 for(let j = 0; j < DIM; j++) {
   let yPos = j * (height/DIM);
-  let row = {};
-  row[`${yPos}`] = [];
+  let row = [];
   for(let i = 0; i < DIM; i++) {
     let xPos = i * ( width/DIM)
-    row[`${yPos}`].push(xPos);
+    row.push(xPos);
     rect(ctx, xPos, yPos, height/DIM, width/DIM);
   }
-  rows.push(row)
+  rows.push(row);
 }
 
+console.log(rows);
 function draw(e){
   let x = getMousePos(canvas, e)['x'];
   let y = getMousePos(canvas, e)['y'];
   mouseYDebug.innerText = `Mouse Y Position: ${y}`;
-  let rowIndex = Math.floor(y / step);
-  console.log(rows[rowIndex]);
+  mouseXDebug.innerText = `Mouse X Position: ${x}`;
+  let columnIndex = Math.floor(x/step)
+  let rowIndex = Math.floor(y/step);
+  // ellipse(ctx, 0, parseInt(rows[rowIndex][0]), 10);
+  // point(ctx, 0, parseInt(Object.keys(rows[rowIndex])[0]));
+  console.log((height/DIM) * rowIndex)
+  // console.log(rows[rowIndex]);
+  // console.log(rows[rowIndex][columnIndex]);
 }
